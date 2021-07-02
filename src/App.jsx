4c9825a3f2fbacc6ef16b53321e638eee9d5d034
Adriel Medeiros <adriel.souza397@gmail.com>
2021-07-02 20:54:17 -0300

    implementing card

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
 import React from "react"
 
+import "./App.css"
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
-
 import Aleatorio from './components/basicos/Aleatorio'
+import Card from "./components/layout/Card"
 
 export default () => (
-  <div id="app">
-    <Primeiro></Primeiro>
-    <ComParametro
-      titulo="Segundo Componente"
-      aluno="Miguel"
-      nota={9.3}
-    >
-    </ComParametro>
+  <div className="App">
+    <div className="Cards">
+      <Card titulo="Primeiro" color="#080">
+        <Primeiro></Primeiro>
+      </Card>
 
-    <Aleatorio
-      max={5}
-      min={1}
-    />
+      <Card titulo="Situação aluno" color="#800">
+        <ComParametro
+          titulo="Segundo Componente"
+          aluno="Miguel"
+          nota={9.3}
+        >
+        </ComParametro>
+
+      </Card>
+      <Card titulo="Aleatorio" color="#006">
+        <Aleatorio
+          max={5}
+          min={1}
+        />
+      </Card>
+    </div>
+
   </div>
 );
