188738187e11815983a3af0c56ab7c4e81ae9ec2
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2021-12-11 07:54:31 -0300

    Criando o componente de cadastro de usuário

---
+++ cadastro-de-usuario/frontend/src/components/user/UserCrud.jsx
@@ -0,0 +1,18 @@
+import react, { Component } from 'react';
+import Main from '../template/Main';
+
+const headerProps = {
+  icon: 'users',
+  title: 'Usuários',
+  subTitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir',
+}
+
+export default class UserCrud extends Component {
+  render() {
+    return (
+      <Main {...headerProps}>
+        cadastro de Usuário
+      </Main>
+    )
+  }
+}
